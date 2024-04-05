b6be0167d2b7347ec0ff131ccf435605a0909d47
Achara Chisom Solomon <ca0102c@gre.ac.uk>
2024-04-05 13:47:45 +0100

    Add error handling for JsonWebTokenError

--- utils/middlewares.js
+++ utils/middlewares.js
@@ -24,6 +24,9 @@ const errorHandler = (error, req, res, next) => {
     if (error.name === 'MongoServerError' && error.message.includes('E11000 duplicate key error')) {
         return res.status(400).json({ error: 'expected `username` to be unique' });
     }
+    if (error.name === 'JsonWebTokenError') {
+        return res.status(401).json({ error: 'missing or invalid token' });
+    }
 
     next(error);
 };
